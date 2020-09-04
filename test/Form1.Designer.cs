29b836b0609ef4b7755fe2f800610377ae860e55
TheRealCain532 <brandoneprintiss@gmail.com>
2020-09-04 12:43:55 -0500

    Add project files.

---
+++ test/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Stat = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.MaxStat = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(323, 38);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Stat,
+            this.MaxStat});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 56);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(323, 445);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // Stat
+            // 
+            this.Stat.HeaderText = "Stat";
+            this.Stat.MaxInputLength = 255;
+            this.Stat.MinimumWidth = 8;
+            this.Stat.Name = "Stat";
+            this.Stat.Width = 75;
+            // 
+            // MaxStat
+            // 
+            this.MaxStat.HeaderText = "Max Stat";
+            this.MaxStat.MaxInputLength = 255;
+            this.MaxStat.MinimumWidth = 8;
+            this.MaxStat.Name = "MaxStat";
+            this.MaxStat.Width = 75;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(342, 297);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(246, 119);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Submit Changes";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(341, 83);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(247, 26);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(341, 143);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(247, 26);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(341, 203);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(247, 26);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(341, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(341, 116);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Hud Text";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(341, 176);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 20);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Nickname";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(600, 510);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.Text = "Smackdown Vs RAW 2010 Save Editor ~ Cain532";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Stat;
+        private System.Windows.Forms.DataGridViewTextBoxColumn MaxStat;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ test/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using svr2010;
+
+namespace test
+{
+    public partial class Form1 : Form
+    {
+        Player p;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog o = new OpenFileDialog();
+            dataGridView1.Rows.Clear();
+            if (o.ShowDialog() == DialogResult.OK)
+            {
+                p = new Player(o.FileName);
+                for (int i = 0; i < p.Stats.Length; i++)
+                    dataGridView1.Rows.Add(p.Stats[i].ToString(), p.MaxStats[i].ToString());
+                textBox1.Text = p.NameText;
+                textBox2.Text = p.HUDText;
+                textBox3.Text = p.Nickname;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            byte[] 
+                newStats = new byte[dataGridView1.Rows.Count-1],
+                newMaxStats = new byte[dataGridView1.Rows.Count - 1];
+            for (int i = 0; i < newStats.Length; i++)
+                if (dataGridView1.Rows[i].Cells[0].Value != null)
+                {
+                    newStats[i] = Convert.ToByte(dataGridView1.Rows[i].Cells[0].Value);
+                    newMaxStats[i] = Convert.ToByte(dataGridView1.Rows[i].Cells[1].Value);
+                }
+            p.Stats = newStats;
+            p.MaxStats = newMaxStats;
+            p.NameText = textBox1.Text;
+            p.HUDText = textBox2.Text;
+            p.Nickname = textBox3.Text;
+        }
+    }
+}
